800fb7c2686f4fe6bf6fa4be62b943ed489b1528
rafaellylemos <rafaellylemos@hotmail.com>
2024-03-19 17:04:56 -0300

    create users list n user interface

---
+++ src/app/data/users-list.ts
@@ -0,0 +1,168 @@
+import { IUser } from "../interfaces/user/user.interface";
+
+export const UsersList: IUser[] = [
+  {
+    nome: "Ana Maria dos Santos",
+    email: "ana@example.com",
+    senha: "senha-segura-123",
+    idade: 34,
+    endereco: {
+      rua: "Rua das Flores",
+      numero: 23,
+      cidade: "São Paulo",
+      estado: "SP",
+      pais: "Brasil",
+      },
+    telefone: "11997654556",
+    ativo: true,
+    funcao: "Desenvolvedor(a) Frontend",
+    dataCadastro: "2023-08-01T09:00:00.000Z",
+    status: {
+      online:true,
+      verificado: true,
+      assinaturaAtiva: true,
+      ultimoAcesso: "2023-08-02T09:00:00:000Z"
+      }
+    },
+  {
+    nome: "Jorge Rafael de Souza Bastos",
+    email: "jorge@example.com",
+    senha: "senha-segura-456",
+    idade: 23,
+    endereco: {
+      rua: "Rua Castro Alvz",
+      numero: 211,
+      cidade: "São Paulo",
+      estado: "SP",
+      pais: "Brasil",
+      },
+    telefone: "11997456387",
+    ativo: true,
+    funcao: "Desenvolvedor(a) Frontend",
+    dataCadastro: "2023-08-01T10:00:00.000Z",
+    status: {
+      online:true,
+      verificado: true,
+      assinaturaAtiva: true,
+      ultimoAcesso: "2023-08-02T10:00:00:000Z"
+      }
+    },
+  {
+        nome: "Carla Almeida Leal",
+        email: "carla@example.com",
+        senha: "senha-segura-789",
+        idade: 34,
+        endereco: {
+          rua: "Rua Ondina",
+          numero: 2334,
+          cidade: "Rio de Janeiro",
+          estado: "RJ",
+          pais: "Brasil",
+          },
+        telefone: "21997654556",
+        ativo: true,
+        funcao: "Desenvolvedor(a) Backend",
+        dataCadastro: "2022-09-11T09:00:00.000Z",
+        status: {
+          online:false,
+          verificado: true,
+          assinaturaAtiva: true,
+          ultimoAcesso: "2023-08-02T09:00:00:000Z"
+          }
+    },
+  {
+          nome: "Patricia Farias Albuquerque",
+          email: "patricia@example.com",
+          senha: "senha-segura-101",
+          idade: 46,
+          endereco: {
+            rua: "Rua Almirante Carlos Porto",
+            numero: 90,
+            cidade: "Recife",
+            estado: "PE",
+            pais: "Brasil",
+            },
+          telefone: "81997774556",
+          ativo: false,
+          funcao: "Gerente RH",
+          dataCadastro: "1999-11-07T09:00:00.000Z",
+          status: {
+            online:false,
+            verificado: true,
+            assinaturaAtiva: false,
+            ultimoAcesso: "2012-03-04T09:00:00:000Z"
+            }
+    },
+  {
+            nome: "Ana Maria dos Santos",
+            email: "ana@example.com",
+            senha: "senha-segura-123",
+            idade: 34,
+            endereco: {
+              rua: "Rua das Flores",
+              numero: 23,
+              cidade: "São Paulo",
+              estado: "SP",
+              pais: "Brasil",
+              },
+            telefone: "11997654556",
+            ativo: true,
+            funcao: "Desenvolvedor(a) Frontend",
+            dataCadastro: "2023-08-01T09:00:00.000Z",
+            status: {
+              online:true,
+              verificado: true,
+              assinaturaAtiva: true,
+              ultimoAcesso: "2023-08-02T09:00:00:000Z"
+              }
+    },
+
+  {
+              nome: "Bernardo Farias de Lima",
+              email: "bernardo@example.com",
+              senha: "senha-segura-124",
+              idade: 22,
+              endereco: {
+                rua: "Rua Carlos Porto Carreiro",
+                numero: 203,
+                cidade: "Salvador",
+                estado: "BA",
+                pais: "Brasil",
+                },
+              telefone: "11997568256",
+              ativo: true,
+              funcao: "Desenvolvedor(a) Salesforce",
+              dataCadastro: "2024-03-19T09:00:00.000Z",
+              status: {
+                online:true,
+                verificado: false,
+                assinaturaAtiva: true,
+                ultimoAcesso: "2024-03-19T09:00:00:000Z"
+                }
+    },
+
+  {
+                nome: "Carla Maria da Silva",
+                email: "carlamaria@example.com",
+                senha: "senha-segura-1230",
+                idade: 55,
+                endereco: {
+                  rua: "Rua Itatiaia",
+                  numero: 10,
+                  cidade: "São Paulo",
+                  estado: "SP",
+                  pais: "Brasil",
+                  },
+                telefone: "11965654556",
+                ativo: true,
+                funcao: "Secretária",
+                dataCadastro: "2002-08-01T09:00:00.000Z",
+                status: {
+                  online: false,
+                  verificado: true,
+                  assinaturaAtiva: true,
+                  ultimoAcesso: "2024-03-18T09:00:00:000Z"
+                  }
+    },
+
+  ]
