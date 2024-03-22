69610dfd448e494925c8c817dc69dab602c27a39
rafaellylemos <rafaellylemos@hotmail.com>
2024-03-22 11:19:04 -0300

    dashEmpty pipe creation

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from "@angular/core";
 import { FormatAdressPipe } from "./endereco.pipe";
 import { PhoneTransformPipe } from "./phone.pipe";
 import { StatusTransformPipe } from "./status.pipe";
+import { DashIfEmptyPipe } from './dash-if-empty.pipe';
 
 @NgModule({
   declarations: [ 
     FormatAdressPipe,
     StatusTransformPipe,
     PhoneTransformPipe,
+    DashIfEmptyPipe,
     ],
   exports: [ 
     FormatAdressPipe,
     StatusTransformPipe,
-    PhoneTransformPipe]
+    PhoneTransformPipe,
+    DashIfEmptyPipe]
 })
 export class PipesModule {
 
