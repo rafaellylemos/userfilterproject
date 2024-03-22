609de935e9b7a157b86e0dc4a4bad26ed5c8cf81
rafaellylemos <rafaellylemos@hotmail.com>
2024-03-22 15:14:05 -0300

    user details (show or hidden)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IUser } from './interfaces/user/user.interface';
-import { UsersList } from './data/users-list';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +7,12 @@ import { UsersList } from './data/users-list';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  userSelected: IUser = UsersList[0];
+
+  showUserDetails:boolean = false;
+  userSelected: IUser = {} as IUser; //o casting aqui foi feito porque, geralmente, quando você vai pegar um objeto, você pega de uma API e ela não vai te retornar o primeiro objeto logo de cara...ai você coloca um objeto vazio... só que o TS não te deixa colocar o objeto vazio porque você tipou ele com a interface IUser e essa interface tem suas propriedades.... ai você faz o casting. Pra dizer que aquele objeto, mesmo sem saber direito o que é, é do tipo IUser.
 
   onUserSelected(user: IUser) {
     this.userSelected = user;
+    this.showUserDetails = true;
   }
 }
